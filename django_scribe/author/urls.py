0d2bb7ed31c016cfc9b73b9a47c6bcf04b8333bd
Nischal Baral <nb06021320@studnent.ku.edu.np>
2022-08-06 16:26:17 +0545

    New server merged

---
+++ django_scribe/author/urls.py
@@ -0,0 +1,10 @@
+from django.urls import URLPattern, path
+
+from . import views
+
+urlpatterns = [
+    path('', views.AuthorAPI.as_view(), name='AuthorAPI'),
+    path('<pk>', views.AuthorInfoAPI.as_view(), name='AuthorInfoAPI'),
+    path('<pk>/books', views.AuthorBooksAPI.as_view(), name='AuthorBooksAPI'),
+    path('<pk>/rating', views.AuthorRatingAPI.as_view(), name='AuthorRatingAPI'),
+]
