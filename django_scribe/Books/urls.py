f2e048af4ec4bdcd29810ff28eae6a52ab901abc
Nischal Baral <nb06021320@studnent.ku.edu.np>
2022-08-06 16:25:00 +0545

    django portion removed

--- django_scribe/Books/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import URLPattern, path
-
-from . import views
-
-urlpatterns = [
-    path('', views.AllBooksAPI.as_view(), name='AllBooksAPI'),
-    path('<pk>',views.BookAPI.as_view(), name='BookAPI' ),
-]
--- django_scribe/py_client/client.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-endpoint = "http://localhost:8000/book"
-endpoint2 = "http://localhost:8000/author"
-
-response= requests.get(endpoint) 
-response2 = requests.get(endpoint2)
-print(response.json())
-print(response2.json())
